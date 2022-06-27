9bee219b5ae3cbade815168d44b860915d4ffba0
nuew <code@nuew.net>
2022-06-27 06:42:52 -0400

    remove old and unused config

--- .config/i3/compositor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ "$XDG_SESSION_TYPE" = "x11" ]; then
-    xinput set-int-prop 8 291 8 1
-    exec xcompmgr -cfCF
-fi
--- .mutt/tagurl_open.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cmd="xdg-open"
-mf="/tmp/mutt-$(id -un)-$(id -u)-url-macros"
-url=$(sed -n "$1 {s/.*'\([^']*\)'.*}/\1/; p}" "$mf")
-$cmd $url
