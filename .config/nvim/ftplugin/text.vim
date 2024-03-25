6414f5ab2fd6ccc774825e56dfe05a46e8056a7f
nuew <code@nuew.net>
2024-03-25 17:18:50 -0400

    Switch from init.vim to init.lua; cleanup text ft options

--- .config/nvim/ftplugin/text.vim
+++ .config/nvim/ftplugin/text.vim
@@ -1,4 +1,4 @@
 setlocal textwidth=100 " 100 char line length
 setlocal spell " enable spell checking
 setlocal complete+=kspell " use spellchecking dictionary for autocompletion
-setlocal noautoindent nocindent nosmartindent indentexpr= " no autoindentation
+setlocal softtabstop=2 shiftwidth=2 " Work better with bulleted lists implemented via comments
---
+++ .config/nvim/init.lua
@@ -0,0 +1,37 @@
+-- General Configuration
+vim.opt.confirm = true -- confirm quit/new file with dialog instead of refusing
+vim.opt.shelltemp = false -- Use pipes instead of shell temporaries where possible
+vim.opt.formatoptions = 'tro/qnj' -- Automatic formatting options; see :help fo-table
+                                  -- we can safely use 't' as all code ftplugins swap
+                                  -- that for 'c' automatically
+vim.opt.guioptions = 'emgrL' -- Graphical mode options; see :help guioptions
+vim.opt.ignorecase = true -- Ignore case when searching
+vim.opt.smartcase = true -- Ignore 'ignorecase' when searching with uppercase characters
+vim.opt.listchars = 'tab:↹·,trail:␢,extends:⇉,precedes:⇇,nbsp:␣' -- Characters for :list
+vim.opt.showbreak = '↳ ' -- String to show for wrapped lines
+vim.opt.undofile = true
+
+-- Set tabs to 4 spaces
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Custom Digraphs; AFAICT there's no native lua interface for this?
+vim.cmd('digraphs o^ 8227') -- U+2023 ‣ TRIANGULAR BULLET
+vim.cmd('digraphs ?! 8253') -- U+204d ‽ INTERROBANG
+vim.cmd('digraphs *: 8258') -- U+2042 ⁂ ASTERISM
+vim.cmd('digraphs h- 8463') -- U+210F ℏ PLANCK CONSTANT OVER TWO PI
+vim.cmd('digraphs @> 8636') -- U+21BB ↻ CLOCKWISE OPEN CIRCLE ARROW
+
+-- Plugin Options
+vim.g.awk_is_gawk = 1
+vim.g.changelog_username = 'Emma Welker <code@nuew.net>'
+vim.g.coq_settings = { auto_start = 'shut-up', xdg = true }
+vim.g.rst_style = 1
+vim.g.rustfmt_autosave = 1
+vim.g.sql_type_default = 'postgresql'
+
+-- Delete undofiles older than one week
+os.execute('/usr/bin/find ' .. vim.opt.undodir:get()[1] .. ' -mtime +7 -delete')
+
+require 'lspconf' -- configure LSP and Coq
