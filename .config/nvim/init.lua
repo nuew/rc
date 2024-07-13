a46d6e2c908d1be033a74552f01f6ba3571388fb
nuew <code@nuew.net>
2024-07-13 15:25:38 -0400

    update misc nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,11 @@
 -- General Configuration
+vim.cmd 'colorscheme industry'
+vim.opt.belloff = ''
 vim.opt.completeopt = 'menu,menuone,preview,noselect' -- insert-mode completion options
 vim.opt.confirm = true -- confirm quit/new file with dialog instead of refusing
 vim.opt.formatoptions = 'tro/qnj' -- Automatic formatting options; see :help fo-table
                                   -- we can safely use 't' as all code ftplugins swap
                                   -- that for 'c' automatically
-vim.opt.guioptions = 'emgrL' -- Graphical mode options; see :help guioptions
 vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.listchars = 'tab:↹·,trail:␢,extends:⇉,precedes:⇇,nbsp:␣' -- Characters for :list
 vim.opt.shelltemp = false -- Use pipes instead of shell temporaries where possible
@@ -18,11 +19,11 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
 -- Custom Digraphs; AFAICT there's no native lua interface for this?
-vim.cmd('digraphs o^ 8227') -- U+2023 ‣ TRIANGULAR BULLET
-vim.cmd('digraphs ?! 8253') -- U+204d ‽ INTERROBANG
-vim.cmd('digraphs *: 8258') -- U+2042 ⁂ ASTERISM
-vim.cmd('digraphs h- 8463') -- U+210F ℏ PLANCK CONSTANT OVER TWO PI
-vim.cmd('digraphs @> 8636') -- U+21BB ↻ CLOCKWISE OPEN CIRCLE ARROW
+vim.cmd 'digraphs o^ 8227' -- U+2023 ‣ TRIANGULAR BULLET
+vim.cmd 'digraphs ?! 8253' -- U+204d ‽ INTERROBANG
+vim.cmd 'digraphs *: 8258' -- U+2042 ⁂ ASTERISM
+vim.cmd 'digraphs h- 8463' -- U+210F ℏ PLANCK CONSTANT OVER TWO PI
+vim.cmd 'digraphs @> 8636' -- U+21BB ↻ CLOCKWISE OPEN CIRCLE ARROW
 
 -- Plugin Options
 vim.g.awk_is_gawk = 1
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -3,7 +3,7 @@ local cmp = require 'cmp'
 
 cmp.setup {
     -- TODO replace with vim.snippet.expand(args.body) for native in v0.10
-    snippet = { expand = function(args) vim.fn['vsnip#anontmous'](args.body) end },
+    snippet = { expand = function(args) vim.fn['vsnip#anonymous'](args.body) end },
     mapping = cmp.mapping.preset.insert {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -39,35 +39,35 @@ cmp.setup.cmdline(':', {
 -- setup individual LSP servers
 local lspconfig = require 'lspconfig'
 local lsp_attach = function(client, buf)
-    buf_setopt = function(opt, action)
+    function buf_setopt(opt, action)
         return vim.api.nvim_buf_set_option(buf, opt, action)
     end
-    buf_setkey = function(key, action, desc)
-        return vim.api.nvim_buf_set_keymap(buf, 'n', key, action, { desc = desc })
+    function buf_setkey(key, action, desc)
+        return vim.keymap.set('n', key, action, { buffer = buf, desc = desc })
     end
     
     buf_setopt('formatexpr', 'v:lua.vim.lsp.formatexpr()')
     buf_setopt('omnifunc', 'v:lua.vim.lsp.omnifunc')
     buf_setopt('tagfunc', 'v:lua.vim.lsp.tagfunc')
 
-    buf_setkey('n', 'gD', vim.lsp.buf.declaration, "Go to Declaration")
-    buf_setkey('n', 'gd', vim.lsp.buf.definition, "Go to Definition")
-    buf_setkey('n', 'gy', vim.lsp.buf.type_definition, "Go to Type Definition")
-    buf_setkey('n', 'K', vim.lsp.buf.hover, "Hover Information")
-    buf_setkey('n', '<leader>ck', vim.lsp.buf.signature_help, "Signature Help")
-    buf_setkey('n', '<leader>qf', vim.diagnostic.setqflist, "Quickfix Diagnostics")
-    buf_setkey('n', '<leader>ql', vim.diagnostic.setloclist, "Diagnostics to Location List")
-    buf_setkey('n', '<leader>qs', vim.diagnostic.show, "Show Diagnostics")
-    buf_setkey('n', '[d', vim.diagnostic.goto_prev, "Previous Diagnostic")
-    buf_setkey('n', ']d', vim.diagnostic.goto_next, "Next Diagnostic")
-    buf_setkey('n', '<leader>e', vim.diagnostic.open_float, "Explain Diagnostic")
-    buf_setkey('n', '<leader>ca', vim.lsp.buf.code_action, "Code Action")
-    buf_setkey('n', '<leader>r', vim.lsp.buf.rename, "Rename")
-    buf_setkey('n', '<leader>fs', vim.lsp.buf.document_symbol, "Document Symbols")
-    buf_setkey('n', '<leader>fs', vim.lsp.buf.workspace_symbol, "Workspace Symbols")
-    buf_setkey('n', '<leader>fi', vim.lsp.buf.implementation, "List Implementations")
-    buf_setkey('n', '<leader>fr', vim.lsp.buf.references, "List References")
-    buf_setkey('n', '<leader>gq', function()
+    buf_setkey('gD', vim.lsp.buf.declaration, "Go to Declaration")
+    buf_setkey('gd', vim.lsp.buf.definition, "Go to Definition")
+    buf_setkey('gy', vim.lsp.buf.type_definition, "Go to Type Definition")
+    buf_setkey('K', vim.lsp.buf.hover, "Hover Information")
+    buf_setkey('<leader>ck', vim.lsp.buf.signature_help, "Signature Help")
+    buf_setkey('<leader>qf', vim.diagnostic.setqflist, "Quickfix Diagnostics")
+    buf_setkey('<leader>ql', vim.diagnostic.setloclist, "Diagnostics to Location List")
+    buf_setkey('<leader>qs', vim.diagnostic.show, "Show Diagnostics")
+    buf_setkey('[d', vim.diagnostic.goto_prev, "Previous Diagnostic")
+    buf_setkey(']d', vim.diagnostic.goto_next, "Next Diagnostic")
+    buf_setkey('<leader>e', vim.diagnostic.open_float, "Explain Diagnostic")
+    buf_setkey('<leader>ca', vim.lsp.buf.code_action, "Code Action")
+    buf_setkey('<leader>r', vim.lsp.buf.rename, "Rename")
+    buf_setkey('<leader>fs', vim.lsp.buf.document_symbol, "Document Symbols")
+    buf_setkey('<leader>fs', vim.lsp.buf.workspace_symbol, "Workspace Symbols")
+    buf_setkey('<leader>fi', vim.lsp.buf.implementation, "List Implementations")
+    buf_setkey('<leader>fr', vim.lsp.buf.references, "List References")
+    buf_setkey('<leader>gq', function()
         vim.lsp.buf.format { async = true }
     end, "Format File")
 end
@@ -76,6 +76,7 @@ lspconfig.util.default_config = vim.tbl_extend('force', lspconfig.util.default_c
     on_attach = lsp_attach
 })
 
+lspconfig.pylsp.setup {}
 lspconfig.clangd.setup {}
 lspconfig.digestif.setup {}
 lspconfig.hls.setup { filetypes = { 'haskell', 'lhaskell', 'cabal' } }
